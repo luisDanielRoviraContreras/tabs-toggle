2b3b82587e992fa85c48e04e9dc41f33b76fc690
luis daniel rovira contreras <luisdanielrovira8@gmail.com>
2017-09-22 05:35:12 -0400

    primer push

---
+++ spec/tabs-toggle-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import TabsToggleView from '../lib/tabs-toggle-view';
+
+describe('TabsToggleView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
